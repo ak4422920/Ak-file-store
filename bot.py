8232450023ca4acb967111408295d507feb824b4
ak4422920 <ak4422920@gmail.com>
2026-03-04 18:05:06 +0530

    Refactor bot.py by removing credits and improving logs
    Removed copyright comments and updated log messages.

--- bot.py
+++ bot.py
@@ -1,16 +1,3 @@
-
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from aiohttp import web
 from plugins import web_server
 import asyncio
@@ -20,7 +7,6 @@ from pyrogram.enums import ParseMode
 import sys
 import pytz
 from datetime import datetime
-#rohit_1888 on Tg
 from config import *
 from database.db_premium import *
 from database.database import *
@@ -38,10 +24,9 @@ async def daily_reset_task():
     try:
         await db.reset_all_verify_counts()
     except Exception:
-        pass  
+        pass
 
 scheduler.add_job(daily_reset_task, "cron", hour=0, minute=0)
-#scheduler.start()
 
 
 def get_indian_time():
@@ -50,14 +35,16 @@ def get_indian_time():
     return datetime.now(ist)
 
 
-name ="""
- BY CODEFLIX BOTS
+name = """
+ BOT SYSTEM
 """
 
+
 def get_indian_time():
     """Returns the current time in IST."""
     ist = pytz.timezone("Asia/Kolkata")
     return datetime.now(ist)
+
 
 class Bot(Client):
     def __init__(self):
@@ -82,39 +69,42 @@ class Bot(Client):
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
+            self.LOGGER(__name__).warning(
+                f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}"
+            )
+            self.LOGGER(__name__).info("\nBot Stopped.")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
-        self.LOGGER(__name__).info(f"""       
+        self.LOGGER(__name__).info("Bot Running..!")
 
-
+        self.LOGGER(__name__).info(f"""
+        
+        
   ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___ 
  / __/ _ \|   \| __| __| |  |_ _\ \/ / _ )/ _ \_   _/ __|
 | (_| (_) | |) | _|| _|| |__ | | >  <| _ \ (_) || | \__ \
  \___\___/|___/|___|_| |____|___/_/\_\___/\___/ |_| |___/
                                                          
- 
-                                          """)
+        """)
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")   
+        self.LOGGER(__name__).info("Bot Running Successfully")
 
         # Start Web Server
         app = web.AppRunner(await web_server())
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
-
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
-        except: pass
+        try:
+            await self.send_message(OWNER_ID, text="<b><blockquote>Bot Restarted Successfully</blockquote></b>")
+        except:
+            pass
 
     async def stop(self, *args):
         await super().stop()
@@ -124,19 +114,10 @@ class Bot(Client):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        self.LOGGER(__name__).info("Bot is now running.")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
             self.LOGGER(__name__).info("Shutting down...")
         finally:
             loop.run_until_complete(self.stop())
-
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
